33e37aeed96efa284771618d1a8be4b3977b0d31
unknown <sandeep.misra96@gmail.com>
2020-12-22 11:45:27 +0530

    Updates

---
+++ precache-manifest.4502d900ccbace84960ecbf75f3faccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2585bc5764f59825bef62bc7f37aaac0",
+    "url": "/resumake-v1/index.html"
+  },
+  {
+    "revision": "c04bcdde62618fe3b78a",
+    "url": "/resumake-v1/static/css/main.44c3bd20.chunk.css"
+  },
+  {
+    "revision": "33d55c1adf97b026dd7f",
+    "url": "/resumake-v1/static/js/2.841e4d52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resumake-v1/static/js/2.841e4d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04bcdde62618fe3b78a",
+    "url": "/resumake-v1/static/js/main.57972e4f.chunk.js"
+  },
+  {
+    "revision": "eebbb5bd3c0989fbb513",
+    "url": "/resumake-v1/static/js/runtime-main.15156ed1.js"
+  }
+]);
